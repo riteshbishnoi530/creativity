f1b479f512159ee92ad824a00c0e39fb82ffd371
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-02-15 12:00:15 +0530

    meta image

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,13 +13,29 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Creativity",
-  description: "Creativity Project Next.js",
-  
+  description: "Creativity Project",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+<meta name="title" content="Creativity" />
+<meta name="description" content="Creativity Project" />
+
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://creativity-ochre.vercel.app/" />
+<meta property="og:title" content="Creativity" />
+<meta property="og:description" content="Creativity Project" />
+<meta property="og:image" content="meta-image.webp" />
+
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://creativity-ochre.vercel.app/" />
+<meta property="twitter:title" content="Creativity" />
+<meta property="twitter:description" content="Creativity Project" />
+<meta property="twitter:image" content="meta-image.webp" />
+
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
